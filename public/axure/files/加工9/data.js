0dec69660c0c49d7f3d7d669a77eaf8f4d7837e4
Gux12 <gx199492@qq.com>
2018-07-05 15:13:13 +0800

    增加axure静态目录

---
+++ public/axure/files/加工9/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B],C,_(D,E,F,G,H,I,J,_(),K,_(L,M,N,O,P,_(Q,R,S,T),U,null,V,O,W,O,X,Y,Z,null,ba,bb,bc,bd,be,bf,bg,bb),bh,_(bi,_()),bj,_(),bk,_(bl,[])),bm,_(),bn,_());}; 
+var b="url",c="加工9.html",d="generationDate",e=new Date(1530770724086.52),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="CommentText",l="SelectedItem",m="FileName",n="SpinnerValue",o="TagName",p="SliderValue",q="CCType",r="PollChoice",s="DroplistOption",t="num",u="AxStWidget",v="AxStXPosition",w="AxStYPosition",x="AxStXLimit",y="AxStYLimit",z="AxStSwipeMomentum",A="AxStPage",B="AxStOrientation",C="page",D="packageId",E="5fda69ec9c634883869653493b595c49",F="type",G="Axure:Page",H="name",I="加工9",J="notes",K="style",L="baseStyle",M="627587b6038d43cca051c114ac41ad32",N="pageAlignment",O="near",P="fill",Q="fillType",R="solid",S="color",T=0xFFFFFFFF,U="image",V="imageHorizontalAlignment",W="imageVerticalAlignment",X="imageRepeat",Y="auto",Z="favicon",ba="sketchFactor",bb="0",bc="colorStyle",bd="appliedColor",be="fontName",bf="Applied Font",bg="borderWidth",bh="adaptiveStyles",bi="a",bj="interactionMap",bk="diagram",bl="objects",bm="masters",bn="objectPaths";
+return _creator();
+})());
